21732129a5697741edc40d732ac16d3e673a1376
kevinGRASSET <ke.grasset@laposte.net>
2017-12-11 13:34:46 +0100

    V0.9

--- Script2.js
+++ Script2.js
@@ -2,21 +2,29 @@
 var tab = new Array("AX","AT","BE","BG","HR","CY","CZ","DK","EE","FO","FI","IM","IT","LV","LT","LU","MT","NL","PL","PT","RO","SK","SI","ES","SE","GB");
 
 
+
 $(document).ready(function()
 {
-	for (var i = 0; i < tab.length; i++) 
+	var k = 0;
+	while(k <= 33)
 	{
-		
-		$.get( "http://api.gbif.org/v1/occurrence/search?species=animalia&country="+tab[i]+"", function(json)
+		$.get( "http://api.gbif.org/v1/occurrence/search?species=animalia&country="+tab[k]+"", function(json)
 	  	{ 
+	  		
 	      $.each(json, function (key, data)
 	      {
 	          $.each(data, function (index, data)
 	          {
-	              $("#"+i+"").append("<a>"+data.scientificName+"<br><br></a>");
-	              i++;
-	          })
+	          	
+	            $("#"+k+"").append("<a>"+data.scientificName+"</a>");
+	            if(data.scientificName == "Pyrrhula pyrrhula")
+	            {
+	            	k++;
+	            }  
+	          })      
 	      })
 	  	})
 	}
+		
+	  	 
 });
