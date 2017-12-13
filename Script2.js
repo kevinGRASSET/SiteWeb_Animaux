5b1ee51746343cbfd2306c3235012ec5fde28b52
kevinGRASSET <grassetk@iut1i-605-a16.local>
2017-12-13 12:05:25 +0100

    V1.0
    Affichage des animaux par pays fonctionnel

--- Script2.js
+++ Script2.js
@@ -1,30 +1,27 @@
 
-var tab = new Array("AX","AT","BE","BG","HR","CY","CZ","DK","EE","FO","FI","IM","IT","LV","LT","LU","MT","NL","PL","PT","RO","SK","SI","ES","SE","GB");
+var tab = new Array("AX","AT","BE","BG","HR","CY","CZ","DK","EE","FO","FI","FR","GF","DE","GI","GR","HU","IE","IM","IT","LV","LT","LU","MT","NL","PL","PT","RO","SK","SI","ES","SE","GB");
 
 
-
-$(document).ready(function()
+function Nom_De_La_Fonction(k) 
 {
-	var k = 0;
-	while(k <= 33)
-	{
-		$.get( "http://api.gbif.org/v1/occurrence/search?species=animalia&country="+tab[k]+"", function(json)
-	  	{ 
-	  		
-	      $.each(json, function (key, data)
-	      {
-	          $.each(data, function (index, data)
-	          {
-	          	
-	            $("#"+k+"").append("<a>"+data.scientificName+"</a>");
-	            if(data.scientificName == "Pyrrhula pyrrhula")
-	            {
-	            	k++;
-	            }  
-	          })      
-	      })
-	  	})
-	}
-		
-	  	 
-});
+		$(document).ready(function()
+		{
+				$.get( "http://api.gbif.org/v1/occurrence/search?species=animalia&country="+tab[k]+"", function(json)
+			  	{ 
+			  		
+			      $.each(json, function (key, data)
+			      {
+			          $.each(data, function (index, data)
+			          {	
+			            $("#"+k+"").append("<a>"+data.scientificName+"<br/><br/></a>"); 
+			          })      
+			      })
+			  	})
+		});
+}
+
+
+for(var i = 0; i <= tab.length; i++)
+{
+	Nom_De_La_Fonction(i)
+}
