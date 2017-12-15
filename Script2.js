af4a61aeb24486fdd4cdf52407c5a57894d15ba5
kevinGRASSET <grassetk@iut1i-605-a16.local>
2017-12-15 12:12:23 +0100

    V1.3

--- Script2.js
+++ Script2.js
@@ -1,19 +1,56 @@
-
 var tab = new Array("AX","AT","BE","BG","HR","CY","CZ","DK","EE","FO","FI","FR","GF","DE","GI","GR","HU","IE","IM","IT","LV","LT","LU","MT","NL","PL","PT","RO","SK","SI","ES","SE","GB");
+var tab2 = new Array("AF","AM","AZ","BH","BD","BT","BN","KH","CN","GE","HK","IN","ID","IR","IQ","IL","JP","JO","KZ","KW","KG","LA","LB","MO","MY","MV","MN","MM","NP","KP","OM","PK","PS","PH","QA","SA","SG","KR","LK","SY","TW","TJ","TH","TL","TR","TM","AE","UZ","VN","YE");
+var tabEurope = [];
+var m = 0;
+var l = 0;
+
+ 
+
+	
+			$.get( "https://restcountries.eu/rest/v2/regionalbloc/eu", function(json)
+            {
+            
+	            $.each(json, function(key, val)
+	            {
+	               tabEurope.push(val.alpha2Code);
+	            	m++;
+	         
+	              
+	            })         
+          	})
+
+          
 
 
-function Nom_De_La_Fonction(k) 
+
+
+
+
+  console.log(tabEurope);
+  console.log(tabEurope[0]);
+ 
+
+
+
+
+
+
+
+
+
+
+function TabEurope(k) 
 {
 		$(document).ready(function()
 		{
-				$.get( "http://api.gbif.org/v1/occurrence/search?species=animalia&country="+tab[k]+"", function(json)
+				$.get( "http://api.gbif.org/v1/occurrence/search?species=animalia&country="+tabEurope[l]+"", function(json)
 			  	{ 
 			  		
 			      $.each(json, function (key, data)
 			      {
 			          $.each(data, function (index, data)
 			          {	
-			            $("#"+k+"").append("<a>"+data.scientificName+"<br/><br/></a>"); 
+			            $("#"+l+"").append("<a>"+data.scientificName+"<br/><br/></a>"); 
 			          })      
 			      })
 			  	})
@@ -21,7 +58,36 @@ function Nom_De_La_Fonction(k)
 }
 
 
+
+
+/*function TabAsia(k) 
+{
+		$(document).ready(function()
+		{
+				$.get( "http://api.gbif.org/v1/occurrence/search?species=animalia&country="+tab2[k]+"", function(json)
+			  	{ 
+			  		
+			      $.each(json, function (key, data)
+			      {
+			          $.each(data, function (index, data)
+			          {	
+			            $("#"+k2+"").append("<a>"+data.scientificName+"<br/><br/></a>"); 
+			          })      
+			      })
+			  	})
+		});
+}*/
+
+
 for(var i = 0; i <= tab.length; i++)
 {
-	Nom_De_La_Fonction(i)
+	TabEurope(i)
 }
+
+
+
+
+
+
+
+
