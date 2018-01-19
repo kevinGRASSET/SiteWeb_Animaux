3be0b9d2c15377b960159e190363fe1d56aa5826
kevinGRASSET <ke.grasset@laposte.net>
2018-01-19 12:08:40 +0100

    V1.4
    tableau auto fonctionnels

--- Script2.js
+++ Script2.js
@@ -1,61 +1,84 @@
 var tab = new Array("AX","AT","BE","BG","HR","CY","CZ","DK","EE","FO","FI","FR","GF","DE","GI","GR","HU","IE","IM","IT","LV","LT","LU","MT","NL","PL","PT","RO","SK","SI","ES","SE","GB");
 var tab2 = new Array("AF","AM","AZ","BH","BD","BT","BN","KH","CN","GE","HK","IN","ID","IR","IQ","IL","JP","JO","KZ","KW","KG","LA","LB","MO","MY","MV","MN","MM","NP","KP","OM","PK","PS","PH","QA","SA","SG","KR","LK","SY","TW","TJ","TH","TL","TR","TM","AE","UZ","VN","YE");
-var tabEurope = [];
+var tabEurope = new Array ();
+
 var m = 0;
-var l = 0;
+
 
  
 
-	
-			$.get( "https://restcountries.eu/rest/v2/regionalbloc/eu", function(json)
+
+		$.get( "https://restcountries.eu/rest/v2/regionalbloc/eu", function(json)
             {
+            	
             
 	            $.each(json, function(key, val)
 	            {
-	               tabEurope.push(val.alpha2Code);
-	            	m++;
-	         
-	              
-	            })         
-          	})
+	            	
+	               tabEurope[m] = ""+val.alpha2Code+"";
+	               m++;
+	            	
+	            	
+	            })
 
-          
+	        console.log(tabEurope);
+	        console.log(tabEurope[0]);
+	        console.log(tabEurope[1]);
+	        TabAsia();
 
-
-
-
-
-
-  console.log(tabEurope);
-  console.log(tabEurope[0]);
- 
+          	});
 
 
+			
 
-
-
-
-
-
-
-
-function TabEurope(k) 
+function TabAsia(k) 
 {
-		$(document).ready(function()
-		{
-				$.get( "http://api.gbif.org/v1/occurrence/search?species=animalia&country="+tabEurope[l]+"", function(json)
-			  	{ 
-			  		
-			      $.each(json, function (key, data)
-			      {
-			          $.each(data, function (index, data)
-			          {	
-			            $("#"+l+"").append("<a>"+data.scientificName+"<br/><br/></a>"); 
-			          })      
-			      })
-			  	})
-		});
-}
+
+	var l = 0;
+
+
+	for (var i = 0; i < tabEurope.length; i++) 
+	{
+  		$.get( "http://api.gbif.org/v1/occurrence/search?species=animalia&country="+tabEurope[i]+"", function(json)
+	  	{ 
+	  		
+	      $.each(json, function (key, data)
+	      {
+	          $.each(data, function (index, data)
+	          {	
+	            $("#"+l+"").append("<a>"+data.scientificName+"<br/><br/></a>"); 
+	            
+	           
+	          }) 
+
+	      })
+			l++;	
+	      
+
+
+	      
+	  	})
+	}
+	
+		
+
+
+	  	 
+};
+			
+
+  		
+	
+
+
+
+
+
+		
+
+
+
+
 
 
 
@@ -78,11 +101,6 @@ function TabEurope(k)
 		});
 }*/
 
-
-for(var i = 0; i <= tab.length; i++)
-{
-	TabEurope(i)
-}
 
 
 
